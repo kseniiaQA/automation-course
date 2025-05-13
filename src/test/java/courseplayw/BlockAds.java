1fa0bd789217c5f90712cd77697b90f1dbc0fca7
Шамтова Ксения Сергеевна <kalekseenko@msc.sigma-it.local>
2025-05-13 15:11:11 +0300

    исправлен .yml

--- src/test/java/courseplayw/BlockAds.java
+++ src/test/java/courseplayw/BlockAds.java
@@ -1,10 +1,16 @@
 package courseplayw;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.WaitForSelectorState;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BlockAds {
     Playwright playwright;
@@ -20,6 +26,32 @@ public class BlockAds {
         context = browser.newContext();
         page = context.newPage();
 
+    }
+
+    @Test
+    void testCartActions() {
+        page.navigate("https://www.komus.ru/katalog/khozyajstvennye-tovary/mylo/c/4118/?from=menu-g2-proizvodstvo_i_sklady");
+        Locator addToButton = page.locator("div.product-price__add-to-cart").nth(0);
+
+
+        addToButton.waitFor(new Locator.WaitForOptions().setTimeout(120000).setState(WaitForSelectorState.VISIBLE));
+        // Добавление товара
+        page.locator("div.product-price__add-to-cart").nth(0).click();
+        page.locator("span.b-icn.b-icn--mini-cart").screenshot(new Locator.ScreenshotOptions()
+                .setPath(getTimestampPath("cart_after_add.png")));
+
+        // Удаление товара
+        page.locator("span.b-icn.b-icn--mini-cart").click();
+        page.locator("div.cart-product-item__remove.gtm-remove-item-fromCart").click();
+        page.locator("a.cart__link.button.button--primary.button--size-m.gtm-empty-cart-to-catalog").click();
+
+        page.locator("span.b-icn.b-icn--mini-cart").screenshot(new Locator.ScreenshotOptions()
+                .setPath(getTimestampPath("cart_after_remove.png")));
+    }
+
+    private Path getTimestampPath(String filename) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
+        return Paths.get("screenshots/", timestamp, filename);
     }
 
 
