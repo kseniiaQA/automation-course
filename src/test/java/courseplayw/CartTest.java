254854bb6346200e8d3390c3bd410a45e4cb6e2d
Шамтова Ксения Сергеевна <kalekseenko@msc.sigma-it.local>
2025-05-13 14:07:29 +0300

    Добавлен тест добавления товара с скринами и видео

---
+++ src/test/java/courseplayw/CartTest.java
@@ -0,0 +1,61 @@
+package courseplayw;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CartTest {
+
+    Playwright playwright;
+    Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeEach
+    void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        context = browser.newContext(new Browser.NewContextOptions()
+                .setRecordVideoDir(Paths.get("videos/")));
+        page = context.newPage();
+    }
+
+    @Test
+    void testCartActions() {
+        page.navigate("https://www.komus.ru/katalog/khozyajstvennye-tovary/mylo/c/4118/?from=menu-g2-proizvodstvo_i_sklady");
+
+        // Добавление товара
+        page.locator("div.product-price__add-to-cart").nth(0).click();
+        page.locator("span.b-icn.b-icn--mini-cart").screenshot(new Locator.ScreenshotOptions()
+                .setPath(getTimestampPath("cart_after_add.png")));
+
+        // Удаление товара
+        page.locator("span.b-icn.b-icn--mini-cart").click();
+        page.locator("div.cart-product-item__remove.gtm-remove-item-fromCart").click();
+        page.locator("a.cart__link.button.button--primary.button--size-m.gtm-empty-cart-to-catalog").click();
+
+        page.locator("span.b-icn.b-icn--mini-cart").screenshot(new Locator.ScreenshotOptions()
+                .setPath(getTimestampPath("cart_after_remove.png")));
+    }
+
+    private Path getTimestampPath(String filename) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
+        return Paths.get("screenshots", timestamp, filename);
+    }
+
+    @AfterEach
+    void teardown() {
+        if (context != null) {
+            context.close();
+        }
+        if (playwright != null) {
+            playwright.close();
+        }
+    }
+}
--- src/test/java/courseplayw/StatusCodeInterceptionTest.java
+++ src/test/java/courseplayw/StatusCodeInterceptionTest.java
@@ -1,61 +1,61 @@
-//package courseplayw;
-//
-//import com.microsoft.playwright.*;
-//import com.microsoft.playwright.options.WaitForSelectorState;
-//import org.junit.jupiter.api.*;
-//
-//import java.util.Collections;
-//
-//public class StatusCodeInterceptionTest {
-//    Playwright playwright;
-//    Browser browser;
-//    BrowserContext context;
-//    Page page;
-//
-//
-//    @BeforeEach
-//    void setUp() {
-//        playwright = Playwright.create();
-//        browser = playwright.chromium().launch();
-//        context = browser.newContext();
-//        page = context.newPage();
-//
-//        // Перехват запроса к /status_codes/404
-//        context.route("**/status_codes/404", route -> {
-//            route.fulfill(new Route.FulfillOptions()
-//                    .setStatus(200) // Устанавливаем статус 200
-//                    .setHeaders(Collections.singletonMap("Content-Type", "text/html"))
-//                    .setBody("<h3>Mocked Success Response</h3>")
-//            );
-//        });
-//    }
-//
-//    @Test
-//    void testMockedStatusCode() {
-//        page.navigate("https://the-internet.herokuapp.com/status_codes");
-//
-//        // Ожидаем ответа перед кликом
-//        Response response = page.waitForResponse("**/status_codes/404", () -> {
-//            // Клик по ссылке "404"
-//            Locator button404 = page.locator("a:has-text('404')");
-//            button404.click();
-//        });
-//
-//        // Проверка мок-текста
-//        String mockedTitle = page.textContent("h3");
-//        Assertions.assertEquals("Mocked Success Response", mockedTitle);
-//
-//        // Проверка статуса ответа
-//        Assertions.assertEquals(200, response.status());
-//    }
-//
-//
-//
-//
-//    @AfterEach
-//    void tearDown() {
-//        context.close();
-//        browser.close();
-//        playwright.close();
-//    }
-//}
+package courseplayw;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.WaitForSelectorState;
+import org.junit.jupiter.api.*;
+
+import java.util.Collections;
+
+public class StatusCodeInterceptionTest {
+    Playwright playwright;
+    Browser browser;
+    BrowserContext context;
+    Page page;
+
+
+    @BeforeEach
+    void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+        context = browser.newContext();
+        page = context.newPage();
+
+        // Перехват запроса к /status_codes/404
+        context.route("**/status_codes/404", route -> {
+            route.fulfill(new Route.FulfillOptions()
+                    .setStatus(200) // Устанавливаем статус 200
+                    .setHeaders(Collections.singletonMap("Content-Type", "text/html"))
+                    .setBody("<h3>Mocked Success Response</h3>")
+            );
+        });
+    }
+
+    @Test
+    void testMockedStatusCode() {
+        page.navigate("https://the-internet.herokuapp.com/status_codes");
+
+        // Ожидаем ответа перед кликом
+        Response response = page.waitForResponse("**/status_codes/404", () -> {
+            // Клик по ссылке "404"
+            Locator button404 = page.locator("a:has-text('404')");
+            button404.click();
+        });
+
+        // Проверка мок-текста
+        String mockedTitle = page.textContent("h3");
+        Assertions.assertEquals("Mocked Success Response", mockedTitle);
+
+        // Проверка статуса ответа
+        Assertions.assertEquals(200, response.status());
+    }
+
+
+
+
+    @AfterEach
+    void tearDown() {
+        context.close();
+        browser.close();
+        playwright.close();
+    }
+}
