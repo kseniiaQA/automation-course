272c6c948fc8306ba395d3ffe71d5d270e1042a9
Шамтова Ксения Сергеевна <kalekseenko@msc.sigma-it.local>
2025-05-21 17:18:48 +0300

    Добавлен тест с авторизацией при помощи local storage и cookie

---
+++ src/test/java/courseplayw/OptimizedLoginTest.java
@@ -0,0 +1,79 @@
+package courseplayw;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.Cookie;
+import com.microsoft.playwright.options.WaitForSelectorState;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.microsoft.playwright.BrowserContext;
+
+public class OptimizedLoginTest {
+
+    static Playwright playwright;
+    static Browser browser;
+    static String backtraceGuid;
+    static String backtraceLastActive;
+
+    // Создаём список cookies
+
+
+    @BeforeAll
+    static void globalSetup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        // Аутентификация один раз перед всеми тестами
+        BrowserContext context = browser.newContext();
+        Page page = context.newPage();
+        page.navigate("https://www.saucedemo.com/");
+        page.locator("#user-name").fill("standard_user");
+        page.locator("#password").fill("secret_sauce");
+        page.locator("#login-button").click();
+
+        // Ждём появления localStorage item 'backtrace-guid' и 'backtrace-last-active'
+        page.waitForFunction("() => localStorage.getItem('backtrace-guid') !== null");
+        backtraceGuid = (String) page.evaluate("() => localStorage.getItem('backtrace-guid')");
+        System.out.println("backtraceGuid: " + backtraceGuid);
+        page.waitForFunction("() => localStorage.getItem('backtrace-last-active') !== null");
+        backtraceLastActive = (String) page.evaluate("() => localStorage.getItem('backtrace-last-active')");
+        context.close();
+    }
+
+    @Test
+    void testDashboard() {
+        // Создаём новый контекст
+        BrowserContext context = browser.newContext();
+        Page page = context.newPage();
+
+        context.addInitScript("localStorage.setItem('backtrace-guid', '" + backtraceGuid + "');");
+        context.addInitScript("localStorage.setItem('backtrace-last-active', '" + backtraceLastActive + "');");
+
+        //добавляем куки перед переходом на страницу
+        List<Cookie> ls = new ArrayList<Cookie>();
+        Cookie cookie = new Cookie("session-username", "standard_user");
+        cookie.setPath("/");
+        cookie.setDomain("www.saucedemo.com");
+        ls.add(cookie);
+        context.addCookies(ls);
+
+        page.navigate("https://www.saucedemo.com/inventory.html");
+
+        Locator cartLink = page.locator("a.shopping_cart_link");
+        cartLink.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+
+        Assertions.assertTrue(cartLink.isVisible());
+
+        context.close();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        browser.close();
+        playwright.close();
+    }
+}
